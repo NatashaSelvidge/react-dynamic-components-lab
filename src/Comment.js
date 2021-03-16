04edb61a81b2004757b5dd61fccd5439340e3a3a
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-16 14:51:45 -0700

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,8 @@
-//your code here
+import { render } from "enzyme";
+import React, { Component } from "react";
+
+export default class Comment extends Component {
+  render() {
+    return <div className="comment">{this.props.commentText}</div>;
+  }
+}
